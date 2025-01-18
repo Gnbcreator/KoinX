d6daabf29aa87cf1e1f237008a06f83db3481c82
Gnbcreator <140880653+Gnbcreator@users.noreply.github.com>
2025-01-18 22:49:59 +0530

    Update TabNavigation.js

--- src/app/componentes/TabNavigation.js
+++ src/app/componentes/TabNavigation.js
@@ -303,7 +303,7 @@ export default function TabNavigation({ total_volume, current, low, high, high52
                                 <Image width={100} height={100} alt="img" className="my-auto mx-3 w-[26px] h-[20px]" src="/accets/SVG_margin.svg" />
                             </div>
                             <div className="flex justify-evenly w-[100%]  lg:w-[830px] lg:gap-[14px] my-7">
-                                <div className="bg-[#EBF9F4] w-[40%] lg:w-[116.86px] lg:h-[120px] rounded-full flex p-4">
+                                <div className="bg-[#EBF9F4] w-[30%] lg:w-[116.86px] md:h-[120px] xl:h-[123px] rounded-full flex p-4">
                                     <div className="mx-auto flex">
                                         <h1 className="my-auto text-[36.41px] font-[500] text-[#08af83]">76</h1>
                                         <span className="my-auto text-[16px] font-[500] text-[#08af83] ">%</span>
